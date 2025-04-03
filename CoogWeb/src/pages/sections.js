be78d092493c7aea0034b4fc6d6c92caa3addbdf
dominiccmcdonald5 <dominiccmcdonald@gmail.com>
2025-04-03 13:04:22 -0500

    Undeploying Vercel

--- CoogWeb/src/pages/sections.js
+++ CoogWeb/src/pages/sections.js
@@ -74,6 +74,7 @@ export const SongCard = ({ song, accountType, userId, setCurrentSong }) => {
     }, [song.image]);
 
     
+
     // Only call fetchInitialLike if accountType is 'user'
     useEffect(() => {
         if (accountType === 'user') {
