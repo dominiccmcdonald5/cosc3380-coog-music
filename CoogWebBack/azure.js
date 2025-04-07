d29c53c1755a21dc39691db68c027b4a408907d3
dominiccmcdonald5 <dominiccmcdonald@gmail.com>
2025-04-06 20:28:51 -0500

    Adding azure.js

---
+++ CoogWebBack/azure.js
@@ -0,0 +1,18 @@
+const { BlobServiceClient } = require("@azure/storage-blob");
+
+const AZURE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
+const CONTAINER_NAME = "mp3";
+
+async function uploadToAzureBlobFromServer(fileBuffer, fileName) {
+    const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_CONNECTION_STRING);
+    const containerClient = blobServiceClient.getContainerClient(CONTAINER_NAME);
+    await containerClient.createIfNotExists({ access: "blob" });
+
+    const blockBlobClient = containerClient.getBlockBlobClient(fileName);
+    await blockBlobClient.uploadData(fileBuffer);
+
+    return `https://musiclibraryfiles.blob.core.windows.net/${CONTAINER_NAME}/${fileName}`;
+
+}
+
+module.exports = { uploadToAzureBlobFromServer };
