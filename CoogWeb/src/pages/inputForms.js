7925b4f136ed08233b1f4392a62361b9747d3fdd
dominiccmcdonald5 <dominiccmcdonald@gmail.com>
2025-04-09 16:03:24 -0500

    Fix Images

--- CoogWeb/src/pages/inputForms.js
+++ CoogWeb/src/pages/inputForms.js
@@ -1041,13 +1041,13 @@ export const ChooseSongCard = ({ song, accountType, userId, setCurrentSong, albu
     return (
         <div className="song-card">
             {/* Render Base64 Image */}
-            {song.image && (
+            
                 <img
                     src={song.image || purple_image}
                     alt={song.name}
                     className="song-image"
                 />
-            )}
+            
 
             <h3 className="song-name">{song.name}</h3>
             <h3 className="song-artist">{song.artist_name}</h3>
